e81d22716776525b03c31f19b8ff4eb526043625
bartoszlysakowski <240739@edu.p.lodz.pl>
2023-04-19 00:54:22 +0200

    6.Zastpanie literalami deklaracjami stalych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,32 +2,41 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int MAX_SIZE = 12;
+    public static final int EMPTY = -1;
+    
+    
+    public static final int FULL = 11;
+    public static final int RETURN_VALUE = -1;
 
-    public int total = -1;
+    private int[] numbers = new int[MAX_SIZE];
+
+
+    public int total = EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return RETURN_VALUE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return RETURN_VALUE;
         return numbers[total--];
     }
 
